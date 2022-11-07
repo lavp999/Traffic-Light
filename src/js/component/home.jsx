2064ebff55359ea2cffaaf617fa2d7ec3e63f757
lavp999 <lavp99@hotmail.com>
2022-11-07 20:22:10 +0000

    casi casi

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,22 +1,36 @@
 import React, { useState } from "react";
+import reactDom from "react-dom";
 
 //create your first component
 const Home = () => {
+
+	const colores =[
+		{rojo: "circulo rojo", verde: "circulo verde", amarillo: "circulo amarillo"},
+		{rojo: "circulo rojo brillo", verde: "circulo verde brillo", amarillo: "circulo amarillo brillo"}
+	]
 	
-	const [ semRojo, setSemRojo] = useState("circulo rojo");
-	const [ semAmarillo, setSemAmarillo] = useState("circulo amarillo");
-	const [ semVerde, setSemVerde] = useState("circulo verde");
+	const [ semRojo, setSemRojo] = useState(colores[0].rojo);
+	const [ semAmarillo, setSemAmarillo] = useState(colores[0].amarillo);
+	const [ semVerde, setSemVerde] = useState(colores[0].verde);
+
+	function desactiva(){
+		setSemRojo(colores[0].rojo);
+		setSemAmarillo(colores[0].amarillo);
+		setSemVerde(colores[0].verde);
+	}
+
 
 	const activaRojo = (evento) =>{
-		setSemRojo("blue");
+		desactiva();
+		setSemRojo(colores[1].rojo);
 	}
 
 	const activaAmarillo = (evento) =>{
-		setSemAmarillo("blue");
+		setSemAmarillo(colores[1].amarillo);
 	}
 
 	const activaVerde = (evento) =>{
-		setSemVerde("blue");
+		setSemVerde(colores[1].verde);
 	}
 
 	return (
